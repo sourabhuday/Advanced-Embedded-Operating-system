60d903cddf7b562a91dea7f2d38ad86ef9ab4e26
Sourabh <sourabhbetigeri@gmail.com>
2017-02-16 02:55:46 -0500

    LAB 3 Grade passed 50/80: Functions to initialize and run a user environment. Also includes trap handling for 0-31 interrupt vectors

--- kern/pmap.c
+++ kern/pmap.c
@@ -163,6 +163,8 @@ mem_init(void)
 	//////////////////////////////////////////////////////////////////////
 	// Make 'envs' point to an array of size 'NENV' of 'struct Env'.
 	// LAB 3: Your code here.
+        envs = (struct Env *)boot_alloc(NENV*sizeof(struct Env));
+       // memset(envs, 0, NENV * sizeof(struct Env));
 
 	//////////////////////////////////////////////////////////////////////
 	// Now that we've allocated the initial kernel data structures, we set
@@ -194,7 +196,7 @@ mem_init(void)
 	//    - the new image at UENVS  -- kernel R, user R
 	//    - envs itself -- kernel RW, user NONE
 	// LAB 3: Your code here.
-
+        boot_map_region(kern_pgdir, UENVS, PTSIZE, PADDR(envs), PTE_U | PTE_P);
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
 	// stack.  The kernel stack grows down from virtual address KSTACKTOP.
